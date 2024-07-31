c974a2fde175284d5cdfc8d4223cc74243cef7c7
paula.bassaganas@boehringer-ingelheim.com <paula.bassaganas@boehringer-ingelheim.com>
2024-07-31 11:13:31 +0200

    trials and echo

--- scripts/modify_tfvars.sh
+++ scripts/modify_tfvars.sh
@@ -8,15 +8,14 @@ fi
 
 branch_name=$(echo "$1" | sed 's/[^a-zA-Z0-9._-]/-/g')
 repo_name=$(echo "$2" | awk -F/ '{print $2}' | sed 's/[^a-zA-Z0-9._-]/-/g')
-echo "pipeline_name=${branch_name}-${repo_name}-codepipeline-$3" >> terraform.tfvars
-
 env=$3
 
+# Update the codepipeline_name variable in terraform.tfvars
+echo "pipeline_name=${branch_name}-${repo_name}-codepipeline-$3" >> ../iac/terraform.tfvars
+
 # Replace placeholders in terraform.tfvars.template and output to terraform.tfvars
-sed -e "s/{branch_name}/$branch_name/" -e "s/{repo_name}/$repo_name/" -e "s/{env}/$env/" terraform.tfvars.template > terraform.tfvars
+sed -e "s/{branch_name}/$branch_name/" -e "s/{repo_name}/$repo_name/" -e "s/{env}/$env/" ../iac/terraform.tfvars.template > ../iac/terraform.tfvars
 
+# Echo the contents of the updated terraform.tfvars for debugging purposes
 echo "Modified terraform.tfvars for branch $branch_name"
-
-# Display the content of terraform.tfvars
-echo "Content of terraform.tfvars:"
-cat terraform.tfvars
+cat ../iac/terraform.tfvars
