74a58ddb0f20e5504dc1ac0bd2ea7495844b9ac4
paula.bassaganas@boehringer-ingelheim.com <paula.bassaganas@boehringer-ingelheim.com>
2024-07-31 11:06:49 +0200

    trials and echo

--- scripts/modify_tfvars.sh
+++ scripts/modify_tfvars.sh
@@ -6,11 +6,17 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-branch_name=$1
-repo_name=$2
+branch_name=$(echo "$1" | sed 's/[^a-zA-Z0-9._-]/-/g')
+repo_name=$(echo "$2" | awk -F/ '{print $2}' | sed 's/[^a-zA-Z0-9._-]/-/g')
+echo "pipeline_name=${branch_name}-${repo_name}-codepipeline-$3" >> terraform.tfvars
+
 env=$3
 
 # Replace placeholders in terraform.tfvars.template and output to terraform.tfvars
 sed -e "s/{branch_name}/$branch_name/" -e "s/{repo_name}/$repo_name/" -e "s/{env}/$env/" terraform.tfvars.template > terraform.tfvars
 
 echo "Modified terraform.tfvars for branch $branch_name"
+
+# Display the content of terraform.tfvars
+echo "Content of terraform.tfvars:"
+cat terraform.tfvars
