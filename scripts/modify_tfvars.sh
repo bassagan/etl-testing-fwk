8bdc8879a7a77ad1e428e9543d768d717dea3b3f
paula.bassaganas@boehringer-ingelheim.com <paula.bassaganas@boehringer-ingelheim.com>
2024-07-31 11:15:54 +0200

    trials and echo

--- scripts/modify_tfvars.sh
+++ scripts/modify_tfvars.sh
@@ -6,18 +6,23 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-ls
-
 branch_name=$(echo "$1" | sed 's/[^a-zA-Z0-9._-]/-/g')
 repo_name=$(echo "$2" | awk -F/ '{print $2}' | sed 's/[^a-zA-Z0-9._-]/-/g')
 env=$3
 
-# Update the codepipeline_name variable in terraform.tfvars
-echo "pipeline_name=${branch_name}-${repo_name}-codepipeline-$3" >> ../iac/terraform.tfvars
+# Define paths relative to the root of the repository
+TEMPLATE_FILE="${GITHUB_WORKSPACE}/iac/terraform.tfvars.template"
+OUTPUT_FILE="${GITHUB_WORKSPACE}/iac/terraform.tfvars"
+
+# Ensure the template file exists
+if [ ! -f "$TEMPLATE_FILE" ]; then
+    echo "Template file not found: $TEMPLATE_FILE"
+    exit 1
+fi
 
 # Replace placeholders in terraform.tfvars.template and output to terraform.tfvars
-sed -e "s/{branch_name}/$branch_name/" -e "s/{repo_name}/$repo_name/" -e "s/{env}/$env/" ../iac/terraform.tfvars.template > ../iac/terraform.tfvars
+sed -e "s/{branch_name}/$branch_name/" -e "s/{repo_name}/$repo_name/" -e "s/{env}/$env/" "$TEMPLATE_FILE" > "$OUTPUT_FILE"
 
 # Echo the contents of the updated terraform.tfvars for debugging purposes
 echo "Modified terraform.tfvars for branch $branch_name"
-cat ../iac/terraform.tfvars
+cat "$OUTPUT_FILE"
