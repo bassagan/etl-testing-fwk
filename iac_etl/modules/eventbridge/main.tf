d985de0e74e14947454ec9008186cd51f067d921
paula.bassaganas@boehringer-ingelheim.com <paula.bassaganas@boehringer-ingelheim.com>
2024-08-12 16:52:54 +0200

    fix id

--- iac_etl/modules/eventbridge/main.tf
+++ iac_etl/modules/eventbridge/main.tf
@@ -6,8 +6,7 @@ resource "aws_cloudwatch_event_rule" "raw_clean_schedule" {
 resource "aws_cloudwatch_event_target" "raw-clean-target" {
   rule      = aws_cloudwatch_event_rule.raw_clean_schedule.name
   target_id = "lambda-target"
-
-  arn = var.raw_clean_function_arn  # Ensure this is passed correctly from the module
+  arn       = var.raw_clean_function_arn  # Ensure this is passed correctly from the module
 
   depends_on = [
     aws_lambda_permission.allow_eventbridge,
@@ -15,13 +14,12 @@ resource "aws_cloudwatch_event_target" "raw-clean-target" {
 }
 
 resource "aws_lambda_permission" "allow_eventbridge" {
-  statement_id  = "AllowExecutionFromEventBridge"
+  statement_id  = "AllowExecutionFromEventBridge-${var.env}-raw-clean"
   action        = "lambda:InvokeFunction"
   function_name = var.raw_clean_function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.raw_clean_schedule.arn
-  depends_on = [var.raw_clean_function_name]
-
+  depends_on    = [var.raw_clean_function_name]
 }
 
 
@@ -33,20 +31,18 @@ resource "aws_cloudwatch_event_rule" "clean_curated_schedule" {
 resource "aws_cloudwatch_event_target" "clean-curated-target" {
   rule      = aws_cloudwatch_event_rule.clean_curated_schedule.name
   target_id = "lambda-target"
-
-  arn = var.clean_curated_function_arn  # Ensure this is passed correctly from the module
+  arn       = var.clean_curated_function_arn  # Ensure this is passed correctly from the module
 
   depends_on = [
-    aws_lambda_permission.allow_eventbridge,
+    aws_lambda_permission.clean_curated_allow_eventbridge,
   ]
 }
 
 resource "aws_lambda_permission" "clean_curated_allow_eventbridge" {
-  statement_id  = "AllowExecutionFromEventBridge"
+  statement_id  = "AllowExecutionFromEventBridge-${var.env}-clean-curated"
   action        = "lambda:InvokeFunction"
   function_name = var.clean_curated_function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.clean_curated_schedule.arn
-  depends_on = [var.clean_curated_function_name]
-
+  depends_on    = [var.clean_curated_function_name]
 }
