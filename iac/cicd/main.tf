b40b0e11fb962469b33d847e3bc08439d71dcfeb
paula.bassaganas@boehringer-ingelheim.com <paula.bassaganas@boehringer-ingelheim.com>
2024-08-23 09:59:26 +0200

    reference properly modules for cicd and init backend on the apply

--- iac/cicd/main.tf
+++ iac/cicd/main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 module "iam" {
-  source = "modules/iam"
+  source = "./modules/iam"
 }
 
 module "codebuild" {
-  source          = "modules/codebuild"
+  source          = "./modules/codebuild"
   codebuild_role  = module.iam.codebuild_role_arn
   github_repo     = var.github_repo
   github_owner    = var.github_owner
@@ -18,7 +18,7 @@ module "codebuild" {
 
 
 module "codepipeline" {
-  source             = "modules/codepipeline"
+  source             = "./modules/codepipeline"
   codepipeline_role  = module.iam.codepipeline_role_arn
   codebuild_project  = module.codebuild.codebuild_project_name
   github_repo        = var.github_repo
