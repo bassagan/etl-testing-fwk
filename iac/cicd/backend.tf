8fe10f4193b534933fabf3421db0ea4931b5b310
paula.bassaganas@boehringer-ingelheim.com <paula.bassaganas@boehringer-ingelheim.com>
2024-08-24 15:55:23 +0200

    new trial for backend new line error

--- iac/cicd/backend.tf
+++ iac/cicd/backend.tf
@@ -1,7 +1,26 @@
 terraform {
   backend "s3" {
-    bucket = "bucket_name_replaced_by_script"
+
+    bucket = "your_bucket_name"
     key            = "etl-testing-fwk/dev/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "dynamo_name_replaced_by_script"  }
+    dynamodb_table = "your_dynamo_db_name"
+
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
