8f96aea0504095ba6a15374d81c659a46c110012
paula.bassaganas@boehringer-ingelheim.com <paula.bassaganas@boehringer-ingelheim.com>
2024-08-25 17:37:40 +0200

    remove management info

--- iac/management/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "paula-bassaganas-manage-users-bucket"
-    key            = "etl-testing-fwk/dev/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "paula-bassaganas-manage-users-dynamodb"
-  }
-}
