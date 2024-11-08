421b892225c8cb2f61bf346af4feb49c221f460c
paula.bassaganas@boehringer-ingelheim.com <paula.bassaganas@boehringer-ingelheim.com>
2024-11-08 21:01:11 +0100

    fix terraform backend

--- iac/etl/backend.tf
+++ iac/etl/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "your_bucket_name"
-    key            = "cicd/terraform.tfstate"
+    key            = "etl/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "your_dynamo_db_name"
   }
